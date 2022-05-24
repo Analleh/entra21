307918007eb4f5231f2121463e4ce24a2567b019
Ana Letícia dos Reis Pereira <alrpereira@furb.br>
2022-05-23 21:50:17 -0300

    23/05

---
+++ POO/src/classes/HotDog.java
@@ -0,0 +1,15 @@
+package classes;
+
+public class HotDog extends Lanche{
+
+    public HotDog(){
+        this.adicionarIngrediente("Pão");
+        this.adicionarIngrediente("Salsicha");
+        this.adicionarIngrediente("Chucrute");
+        this.adicionarIngrediente("Batata Palha");
+        this.adicionarIngrediente("Vinagrete");
+        this.adicionarIngrediente("Queijo Cheddar");
+        this.adicionarIngrediente("Milho");
+        this.adicionarIngrediente("Ervilha");
+    }
+}
