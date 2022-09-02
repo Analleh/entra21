575f2fb496bbfd438f697bba01ee7e504e945d38
Ana Letícia dos Reis Pereira <alrpereira@furb.br>
2022-09-01 21:35:28 -0300

    aula 01/09

--- Spring/primeiro-projeto-spring/src/main/java/com/entra21/primeiroprojetospring/model/entity/FranquiaEntity.java
+++ Spring/primeiro-projeto-spring/src/main/java/com/entra21/primeiroprojetospring/model/entity/FranquiaEntity.java
@@ -1,7 +1,18 @@
 package com.entra21.primeiroprojetospring.model.entity;
 
-import javax.persistence.Entity;
+import lombok.Data;
 
-@Entity
+import javax.persistence.*;
+
+@Data
+@Entity // entidade do banco de dados
+@Table (name = "franquia") // informar qual tabela
 public class FranquiaEntity {
+    @Column(name = "id") // informar qual coluna da tabela
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nome", nullable = false)
+    private String nome;
 }
